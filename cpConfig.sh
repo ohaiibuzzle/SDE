410831106e0368c7bfe51dde5646639ac6e51997
simsid66 <simsid66@gmail.com>
2019-11-19 02:31:32 +0530

    Began working on getting rid of openbox's startup and stuff.

--- cpConfig.sh
+++ cpConfig.sh
@@ -17,37 +17,48 @@ if [ -d "$HOME/.config/obmenu-generator" ]; then
 else
     mkdir ~/.config/obmenu-generator
 fi
+
+echo -n "->"
 echo "openbox/rc.xml"
 cp "$PWD/rc.xml" "$HOME/.config/openbox/rc.xml"
 
+echo -n "->"
 echo "openbox/autostart"
 cp "$PWD/autostart" "$HOME/.config/openbox/autostart"
 
+echo -n "->"
 echo "openbox/menu.xml"
 cp "$PWD/menu.xml" "$HOME/.config/openbox/menu.xml"
 
+echo -n "->"
 echo "tint2/tint2rc"
 cp "$PWD/tint2rc" "$HOME/.config/tint2/tint2rc"
 
+echo -n "->"
 echo "init.sh"
 cp "$PWD/init.sh" "$HOME/init.sh"
 chmod +x "$HOME/init.sh"
 
+echo -n "->"
 echo "randomWallpaper.sh"
 cp "$PWD/randomWallpaper.sh" "$HOME/randomWallpaper.sh"
 chmod +x "$HOME/randomWallpaper.sh"
 
+echo -n "->"
 echo "setWall.sh"
 cp "$PWD/setWall.sh" "$HOME/setWall.sh"
 chmod +x "$HOME/setWall.sh"
 
+echo -n "->"
 echo "lemon.sh"
 cp "$PWD/lemon.sh" "$HOME/lemon.sh"
 chmod +x "$HOME/lemon.sh"
 
+echo -n "->"
 echo "volume_status.py"
 cp "$PWD/volume_status.py" "$HOME/volume_status.py"
 
+echo -n "->"
 echo "obmenu-generator/schema.pl"
 cp "$PWD/schema.pl" "$HOME/.config/obmenu-generator/schema.pl"
 
--- install.py
+++ install.py
@@ -85,7 +85,7 @@ depends = {
 }
 
 def download_file(url, filename, location):
-    print("downloading " + filename + " from " + url "...")
+    print("downloading " + filename + " from " + url + "...")
     r = requests.get(url)
     with open(location + filename, 'wb') as f:
         f.write(r.content)
@@ -133,4 +133,4 @@ for depend in ['openbox', 'obmenu-generator', 'xfce4-terminal', 'tint2', 'xcompm
 print("copying configs...")
 subprocess.run(['sh', cwd+"cpConfig.sh"])
 
-print("SDE install completed successfully. You should create and populate ~/wallpapers now")
+print("SDE install completed successfully. You should create and populate ~/wallpapers now.")
